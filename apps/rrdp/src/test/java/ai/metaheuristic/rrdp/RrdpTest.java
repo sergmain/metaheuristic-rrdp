f56192e8ed63eaac3cfd0f889aedfa3461807993
sergmain <serg_main@yahoo.com>
2022-06-10 00:57:13 -0700

    1st version is finished

--- apps/rrdp/src/test/java/ai/metaheuristic/rrdp/RrdpTest.java
+++ apps/rrdp/src/test/java/ai/metaheuristic/rrdp/RrdpTest.java
@@ -21,13 +21,11 @@ public class RrdpTest {
         String session = UUID.randomUUID().toString();
         int serial = 1;
 
-        StringWriter snapshot = new StringWriter();
-        StringWriter delta = new StringWriter();
+        StringWriter notificationEntry = new StringWriter();
 
         RrdpEntry en1 = publishEntry("entry #1", "http://uri1");
         RrdpEntry en2 = publishEntry("entry #2", "http://uri2");
         RrdpEntry en3 = publishEntry("entry #3", "http://uri3");
-        RrdpEntry en4 = publishEntry("entry #4", "http://uri4");
 
 
         //noinspection ReturnOfNull
@@ -37,31 +35,29 @@ public class RrdpTest {
                 .withCurrentNotification(()->null)
                 .withRrdpEntryIterator(()-> List.of(en1, en2, en3).iterator())
                 .withCurrSerial((s)->serial)
-                .withPersistNotificationEntry(snapshot::write)
-                .withPersistNotificationEntry(delta::write)
+                .withPersistNotificationEntry(notificationEntry::write)
                 .withProduceType(()-> RrdpEnums.NotificationEntryType.SNAPSHOT);
 
         Rrdp rrdp = new Rrdp(cfg);
         rrdp.produce();
 
         StringWriter notification = new StringWriter();
-        rrdp.produceNotification(new UriAndHash("http://notification-snapshot", DigestUtils.sha256Hex(snapshot.toString())), notification::write);
+        final UriAndHash uriAndHash = new UriAndHash("http://notification-snapshot", DigestUtils.sha256Hex(notificationEntry.toString()));
+        rrdp.produceNotification(uriAndHash, notification::write);
 
-        String snapshotXml = snapshot.toString();
-        String deltaXml = delta.toString();
+        String deltaXml = notificationEntry.toString();
         String notificationXml = notification.toString();
 
-        System.out.println(snapshotXml);
+        System.out.println(deltaXml);
         System.out.println(notificationXml);
 
-        assertNotEquals(0, snapshotXml.length());
-        assertEquals(0, deltaXml.length());
+        assertNotEquals(0, deltaXml.length());
         assertNotEquals(0, notificationXml.length());
 
-        verifyNotification(notificationXml, session, en4);
+        verifyNotification(notificationXml, session, uriAndHash);
     }
 
-    private static void verifyNotification(String notificationXml, String session, RrdpEntry en4) {
+    private static void verifyNotification(String notificationXml, String session, UriAndHash uriAndHash) {
         Notification n = RrdpUtils.parseNotificationXml(notificationXml);
 
         assertEquals(session, n.sessionId);
@@ -72,8 +68,8 @@ public class RrdpTest {
 
         assertNull(e1.serial);
         assertEquals(RrdpEnums.NotificationEntryType.SNAPSHOT, e1.type);
-        assertEquals(en4.hash.get(), e1.hash);
-        assertEquals(en4.uri.get(), e1.uri);
+        assertEquals(uriAndHash.hash, e1.hash);
+        assertEquals(uriAndHash.uri, e1.uri);
     }
 
     @Test
@@ -82,13 +78,11 @@ public class RrdpTest {
         int serial = 2;
 
         StringWriter notificationEntry = new StringWriter();
-        StringWriter notification = new StringWriter();
 
         RrdpEntry en1 = publishEntry("entry #1", "http://uri1");
         RrdpEntry en2 = publishEntry("entry #2", "http://uri2");
         RrdpEntry en3 = publishEntry("entry #3", "http://uri3");
         RrdpEntry en4 = withdrawEntry("entry #4", "http://uri4");
-        RrdpEntry en5 = withdrawEntry("entry #5", "http://uri5");
 
         //noinspection ReturnOfNull
         RrdpConfig cfg = new RrdpConfig()
@@ -104,13 +98,10 @@ public class RrdpTest {
         rrdp.produce();
 
         String notificationEntryXml = notificationEntry.toString();
-        String notificationXml = notification.toString();
 
         System.out.println(notificationEntryXml);
-        System.out.println(notificationXml);
 
         assertNotEquals(0, notificationEntryXml.length());
-        assertNotEquals(0, notificationXml.length());
     }
 
     private static RrdpEntry publishEntry(String content, String uri) {
